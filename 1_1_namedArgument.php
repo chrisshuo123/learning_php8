40200e45ea80ae8afbe975b75d58e75a71bcfb46
chrisshuo123 <chr1z100@gmail.com>
2025-10-14 21:15:52 +0700

    2 new files on Learning PHP 8.0 part 1: Named Argument from featureA branch

---
+++ 1_1_namedArgument.php
@@ -0,0 +1,23 @@
+<?php
+
+// 1.1. Named Argument
+function sayHello(string $firstName, string $middleName, string $lastName): void {
+    echo "Hello, nama saya $firstName $middleName $lastName" . PHP_EOL;
+}
+
+// Pada PHP versi 7 (PHP 7.0) isinya harus urut dengan parameter, tidak bisa dibalik - balik.
+// Contoh Kasus:
+sayHello("Owen", "Baguete", "Jelman");
+// Output: Hello, nama saya Owen Baguete Jelman
+// Itu sudah fiks sesuai urutan parameter dimulai nama depan, tengah, dan belakang.
+
+// Pada PHP versi 8 (PHP 8.0), terdapat fitur namanya Named Argument
+// Caranya cukup memanggil parameter dari methodnya:
+sayHello(middleName:"Owen", firstName:"Baguete", lastName:"Jelman");
+// Output: Hello, nama saya Baguete Owen Jelman
+
+// Salah satu keuntungan menggunakan Named Argument: 
+// Ketika kita bikin Default Argument
+
+
+?>
